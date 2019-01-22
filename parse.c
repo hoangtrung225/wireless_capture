f68c8031b6d545ac5e06a3f85f6e865803108f24
chaosbringer <chaosbringer@chaos-debian.chaos-laptop>
2019-01-22 23:32:11 +0700

    modify code base make demo example

--- parse.c
+++ parse.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <pcap.h>
 #if defined(__APPLE__)
 #include <machine/endian.h>
 #else
@@ -13,6 +14,8 @@
 #endif
 
 #include "radiotap_iter.h"
+
+#define SELECT_FILTER "wlan host "
 
 static int fcshdr = 0;
 
@@ -39,35 +42,36 @@ static void print_radiotap_namespace(struct ieee80211_radiotap_iterator *iter)
 {
 	switch (iter->this_arg_index) {
 	case IEEE80211_RADIOTAP_TSFT:
-		printf("\tTSFT: %llu\n", le64toh(*(unsigned long long *)iter->this_arg));
 		break;
 	case IEEE80211_RADIOTAP_FLAGS:
-		printf("\tflags: %02x\n", *iter->this_arg);
 		break;
 	case IEEE80211_RADIOTAP_RATE:
-		printf("\trate: %lf\n", (double)*iter->this_arg/2);
 		break;
 	case IEEE80211_RADIOTAP_CHANNEL:
+		break;
 	case IEEE80211_RADIOTAP_FHSS:
+		break;
 	case IEEE80211_RADIOTAP_DBM_ANTSIGNAL:
+		printf("\ttin hieu antenna: %ddbm\tkhoang cach voi AccessPoint: %dm\n", *iter->this_arg - 256, (256 - *iter->this_arg) / 10);
+		break;
 	case IEEE80211_RADIOTAP_DBM_ANTNOISE:
+		printf("\tantenna noise receive\n");
+		break;
 	case IEEE80211_RADIOTAP_LOCK_QUALITY:
+		break;
 	case IEEE80211_RADIOTAP_TX_ATTENUATION:
+		printf("\ttransmite antenna signal: %d\n", *iter->this_arg - 256);
+		break;
 	case IEEE80211_RADIOTAP_DB_TX_ATTENUATION:
 	case IEEE80211_RADIOTAP_DBM_TX_POWER:
+		printf("\ttransmite antenna signal: %d\n", *iter->this_arg - 256);
+		break;
 	case IEEE80211_RADIOTAP_ANTENNA:
 	case IEEE80211_RADIOTAP_DB_ANTSIGNAL:
 	case IEEE80211_RADIOTAP_DB_ANTNOISE:
 	case IEEE80211_RADIOTAP_TX_FLAGS:
 		break;
 	case IEEE80211_RADIOTAP_RX_FLAGS:
-		if (fcshdr) {
-			printf("\tFCS in header: %.8x\n",
-				le32toh(*(uint32_t *)iter->this_arg));
-			break;
-		}
-		printf("\tRX flags: %#.4x\n",
-			le16toh(*(uint16_t *)iter->this_arg));
 		break;
 	case IEEE80211_RADIOTAP_RTS_RETRIES:
 	case IEEE80211_RADIOTAP_DATA_RETRIES:
@@ -98,72 +102,97 @@ static const struct radiotap_override overrides[] = {
 	{ .field = 14, .align = 4, .size = 4, }
 };
 
+int print_help(void){
+	fprintf(stderr, "parse [interface] [MACaddr]\n");
+	return -1;
+}
+
 int main(int argc, char *argv[])
 {
+       	pcap_t *handle;			/* Session handle */
+       	char *dev; 			/* The device to sniff on */
+       	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+       	struct pcap_pkthdr header;	/* The header that pcap gives us */
+       	const u_char *packet;		/* The actual packet */
+       	int err, i;
 	struct ieee80211_radiotap_iterator iter;
-	struct stat statbuf;
-	int fd, err, fnidx = 1, i;
-	void *data;
+        /* Define the device */
+        //dev = pcap_lookupdev(errbuf);
+	if(argc != 3)
+		return -1;
 
-	if (argc != 2 && argc != 3) {
-		fprintf(stderr, "usage: parse [--fcshdr] <file>\n");
-		fprintf(stderr, "	--fcshdr: read bit 14 as FCS\n");
-		return 2;
-	}
+	dev = argv[1];
+        if (dev == NULL) {
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		print_help();
+         	return(2);
+        }
 
-	if (strcmp(argv[1], "--fcshdr") == 0) {
-		fcshdr = 1;
-		fnidx++;
-	}
+        /* Open the session in promiscuous mode */
+        handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+        if (handle == NULL) {
+         	fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+         	return(2);
+        }
+        pcap_set_snaplen(handle, 2048);  // Set the snapshot length to 2048
+        pcap_set_promisc(handle, 1);     // Turn promiscuous mode off
+        pcap_set_timeout(handle, 512);   // Set the timeout to 512 milliseconds
+        int status = pcap_activate(handle);
+        
+        if(pcap_set_rfmon(handle,1)==0 )
+        {
+         	printf("monitor mode enabled\n");
+        }
 
-	fd = open(argv[fnidx], O_RDONLY);
-	if (fd < 0) {
-		fprintf(stderr, "cannot open file %s\n", argv[fnidx]);
-		return 2;
-	}
+        if(pcap_set_datalink(handle, DLT_IEEE802_11_RADIO) == -1) {
+         	printf("Couldn't set datalink type %s: %s\n", dev, pcap_geterr(handle));
+        }
 
-	if (fstat(fd, &statbuf)) {
-		perror("fstat");
-		return 2;
-	}
+	char filter[64];
+	memset(filter, 0, 64);
+       	strncpy(filter, SELECT_FILTER, strlen(SELECT_FILTER));
+       	strncpy(filter + strlen(SELECT_FILTER), argv[2], strlen(argv[2]));	// beacon frame WLAN
+	fprintf(stderr, "Selected filter string: %s\n", filter);
+	struct bpf_program fp; 
+	bpf_u_int32 netp; // Berkley Packet Filter (same as u_int32_t i believe)
+	if(pcap_compile(handle,&fp,filter,0,netp)==-1) // -1 means failed
+		fprintf(stderr,"Error compiling Libpcap filter, %s\n",filter);
+	if(pcap_setfilter(handle,&fp)==-1) // -1 means failed - but we don't exit(1)
+		fprintf(stderr,"Error setting Libpcap filter, %s\n",filter); // same as above
 
-	data = mmap(NULL, statbuf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	printf("Type: %d\n",pcap_datalink(handle));
 
-	err = ieee80211_radiotap_iterator_init(&iter, data, statbuf.st_size, &vns);
+	while((packet = pcap_next(handle, &header)) != NULL) {
+	/* Print its length */
+//	printf("Jacked a packet with length of [%d]\n", header.len);
+	err = ieee80211_radiotap_iterator_init(&iter, packet, header.len, &vns);
 	if (err) {
-		printf("malformed radiotap header (init returns %d)\n", err);
-		return 3;
+	       printf("malformed radiotap header (init returns %d)\n", err);
+	       return 3;
 	}
 
-	if (fcshdr) {
-		iter.overrides = overrides;
-		iter.n_overrides = sizeof(overrides)/sizeof(overrides[0]);
-	}
+         	while (!(err = ieee80211_radiotap_iterator_next(&iter))) {
+         	if (iter.this_arg_index == IEEE80211_RADIOTAP_VENDOR_NAMESPACE) {
+         		printf("\tvendor NS (%.2x-%.2x-%.2x:%d, %d bytes)\n",
+         			iter.this_arg[0], iter.this_arg[1],
+         			iter.this_arg[2], iter.this_arg[3],
+         			iter.this_arg_size - 6);
+         		for (i = 6; i < iter.this_arg_size; i++) {
+         			if (i % 8 == 6)
+         				printf("\t\t");
+         			else
+         				printf(" ");
+         			printf("%.2x", iter.this_arg[i]);
+         		}
+         		printf("\n");
+         	} else if (iter.is_radiotap_ns)
+         		print_radiotap_namespace(&iter);
+         	else if (iter.current_namespace == &vns_array[0])
+         		print_test_namespace(&iter);
+         }
 
-	while (!(err = ieee80211_radiotap_iterator_next(&iter))) {
-		if (iter.this_arg_index == IEEE80211_RADIOTAP_VENDOR_NAMESPACE) {
-			printf("\tvendor NS (%.2x-%.2x-%.2x:%d, %d bytes)\n",
-				iter.this_arg[0], iter.this_arg[1],
-				iter.this_arg[2], iter.this_arg[3],
-				iter.this_arg_size - 6);
-			for (i = 6; i < iter.this_arg_size; i++) {
-				if (i % 8 == 6)
-					printf("\t\t");
-				else
-					printf(" ");
-				printf("%.2x", iter.this_arg[i]);
-			}
-			printf("\n");
-		} else if (iter.is_radiotap_ns)
-			print_radiotap_namespace(&iter);
-		else if (iter.current_namespace == &vns_array[0])
-			print_test_namespace(&iter);
-	}
-
-	if (err != -ENOENT) {
-		printf("malformed radiotap data\n");
-		return 3;
-	}
-
-	return 0;
+        }
+        /* And close the session */
+        pcap_close(handle);
+        return(0);
 }
